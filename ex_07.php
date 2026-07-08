b98394f34a41208bea6791469359ac705e934f3c
franciscohgoulart <francisco_h_machado@estudante.sesisenai.org.br>
2026-07-08 09:47:17 -0300

    add- metade da atividade 7

---
+++ ex_07.php
@@ -0,0 +1,38 @@
+<?php
+function calcularDesconto($valor) {
+
+if ($valor <= 100) {
+    $desconto = 0;
+}
+
+elseif ($valor > 100) {
+    $desconto = $valor * 0.10;
+}
+
+elseif ($valor > 500) {
+    $desconto = $valor * 0.20;
+}
+
+elseif ($valor > 1000)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+?>
