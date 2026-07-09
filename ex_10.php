6f8bdc3def9196aa881ee7cdd27efdf05f6d968d
franciscohgoulart <francisco_h_machado@estudante.sesisenai.org.br>
2026-07-09 09:09:55 -0300

    add- exercicio 10 finalizado

--- ex_10.php
+++ ex_10.php
@@ -1,10 +1,36 @@
 <?php
 function calcularMedia($notas) {
-    $soma = array_sum($notas);
-    $quantidade = count($notas);
-    $media = $soma / $quantidade;
-    return $media;
+   
+    if (empty($notas)) {
+        return "Erro: nenhum nota informada.";
+    }
+
+    $maiorNota = max($notas);
+    $menorNota = min($notas);
+    $media = array_sum($notas) / count($notas);
+
+    if ($media >= 7) {
+        $situacao = "Aprovado";
+    } elseif ($media >= 5) {
+        $situacao = "Recuperação";
+    } else {
+        $situacao = "Reprovado";
+    }
+
+    return [
+        "maior_nota" => $maiorNota,
+        "menor_nota" => $menorNota,
+        "media"      => round($media, 2),
+        "situacao"   => $situacao
+    ];
 }
 
 
+$notasAluno = [7.5, 8.0, 6.5, 9.0];
+$resultado = calcularMedia($notasAluno);
+
+echo "Maior nota: " . $resultado["maior_nota"] . "\n";
+echo "Menor nota: " . $resultado["menor_nota"] . "\n";
+echo "Média: " . $resultado["media"] . "\n";
+echo "Situação: " . $resultado["situacao"] . "\n";
 ?>
