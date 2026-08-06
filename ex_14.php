17dabdf0352b7bb50af0383ef0b2b192a751aa69
franciscohgoulart <francisco_h_machado@estudante.sesisenai.org.br>
2026-08-06 09:52:48 -0300

    add- 14 finalizada

---
+++ ex_14.php
@@ -0,0 +1,49 @@
+<?php
+function estatisticasNumericas($numeros){
+
+    $soma = 0;
+    $pares = 0;
+    $impares = 0;
+
+    $maior = $numeros[0];
+    $menor = $numeros[0];
+
+    foreach($numeros as $numero){
+
+        $soma += $numero;
+
+        if($numero > $maior){
+            $maior = $numero;
+        }
+
+        if($numero < $menor){
+            $menor = $numero;
+        }
+
+        if($numero % 2 == 0){
+            $pares++;
+        }else{
+            $impares++;
+        }
+    }
+
+    $media = $soma / count($numeros);
+
+    sort($numeros);
+
+    $quantidade = count($numeros);
+
+    if($quantidade % 2 == 0){
+        $mediana = ($numeros[$quantidade / 2 - 1] + $numeros[$quantidade / 2]) / 2;
+    }else{
+        $mediana = $numeros[floor($quantidade / 2)];
+    }
+
+    return "Soma: $soma<br>" .
+           "Média: $media<br>" .
+           "Maior valor: $maior<br>" .
+           "Menor valor: $menor<br>" .
+           "Mediana: $mediana<br>" .
+           "Quantidade de números pares: $pares<br>" .
+           "Quantidade de números ímpares: $impares";
+}
